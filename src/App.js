5c452d77ecb7a47c1b9d6c8b7604ce7af1c2c175
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-02-12 15:51:54 +0530

    UtilityCharges

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SearchBar from "./Component/SearchBar/SearchBar";
 import AddNewButton from "./Component/Buttons/AddNewButton";
 import MemberList from "./Pages/ResidentInfoPage/Member List/MemberList";
 import ResidentsPayments from "./Pages/FinancePage/ResidentsPayments/ResidentsPayments";
+import UtilityCharges from "./Pages/FinancePage/UtilityCharges/UtilityCharges";
 
 function App() {
   return (
@@ -28,7 +29,8 @@ function App() {
         {/* <UnitList /> */}
         {/* <StaffList/> */}
         {/* <AddNewButton/> */}
-        <ResidentsPayments/>
+        {/* <ResidentsPayments/> */}
+        <UtilityCharges/>
       </div>
     </>
   );
