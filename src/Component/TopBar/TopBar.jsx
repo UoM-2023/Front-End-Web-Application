8d1f602abcc61bc82c0bc77da71f261d6ed08e29
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-06-15 00:53:55 +0530

    User Popover

--- src/Component/TopBar/TopBar.jsx
+++ src/Component/TopBar/TopBar.jsx
@@ -1,95 +1,3 @@
-// import React from "react";
-// import "./TopBar.css";
-// import AppBar from "@mui/material/AppBar";
-// import Box from "@mui/material/Box";
-// import Toolbar from "@mui/material/Toolbar";
-// import Typography from "@mui/material/Typography";
-// import IconButton from "@mui/material/IconButton";
-// import AccountCircle from "@mui/icons-material/AccountCircle";
-// import MenuItem from "@mui/material/MenuItem";
-// import Menu from "@mui/material/Menu";
-
-// const TopBar = (props) => {
-//   const [auth, setAuth] = React.useState(true);
-//   const [anchorEl, setAnchorEl] = React.useState(null);
-
-//   const handleMenu = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//   };
-//   console.log(props);
-//   return (
-//     <div className="topBarContainer">
-//       <Box sx={{ flexGrow: 1 }}>
-//         <AppBar position="static" sx={{ backgroundColor: "#fffffff7" }}>
-//           <Toolbar>
-//             <Typography
-//               component="div"
-//               sx={{
-//                 flexGrow: 1,
-//                 color: "#000000",
-//                 fontWeight: "bold",
-//                 fontSize: "23px",
-//                 marginLeft: "5rem",
-//               }}
-//             >
-//               {props.title}
-//             </Typography>
-//             {auth && (
-//               <div>
-//                 <IconButton
-//                   aria-label="account of current user"
-//                   aria-controls="menu-appbar"
-//                   aria-haspopup="true"
-//                   onClick={handleMenu}
-//                   size="large"
-//                   sx={{ padding: "0.6rem" }}
-//                 >
-//                   <AccountCircle
-//                     sx={{
-//                       color: "#E76736",
-//                       width: "3rem",
-//                       height: "3rem",
-//                     }}
-//                   />
-//                 </IconButton>
-//                 <Menu
-//                   id="menu-appbar"
-//                   anchorEl={anchorEl}
-//                   anchorOrigin={{
-//                     vertical: "bottom",
-//                     horizontal: "right",
-//                   }}
-//                   keepMounted
-//                   transformOrigin={{
-//                     vertical: "top",
-//                     horizontal: "right",
-//                   }}
-//                   open={Boolean(anchorEl)}
-//                   onClose={handleClose}
-//                 >
-//                   <MenuItem onClick={handleClose}>Your Profile</MenuItem>
-//                   <MenuItem onClick={handleClose}>Settings</MenuItem>
-//                   <MenuItem onClick={handleClose} sx={{ fontWeight: "bold" }}>
-//                     Logout
-//                   </MenuItem>
-//                 </Menu>
-//               </div>
-//             )}
-//           </Toolbar>
-//         </AppBar>
-//       </Box>
-//     </div>
-//   );
-// };
-
-// export default TopBar;
-
-//--------------------------------------------------------------------------------------------------------------------------------------
-
 import React from "react";
 import "./TopBar.css";
 import AppBar from "@mui/material/AppBar";
@@ -101,7 +9,6 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import LogoutIcon from "@mui/icons-material/Logout";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
 const TopBar = (props) => {
@@ -168,7 +75,7 @@ const TopBar = (props) => {
                 >
                   <div style={{ padding: "1rem" }}>
                     <MenuItem onClick={handleClose}>
-                      <div className="Login User Details">
+                      <div className="LoginUserDetails">
                         {/* <span className="textItem">
                           <b>Name :</b> A.B.C.Kulathilake
                         </span> */}
@@ -179,19 +86,16 @@ const TopBar = (props) => {
                           <b>Staff ID :</b> S-254109
                         </span> */}
                         <span className="textItem">
-                          <b>User Role :</b> Finance Manager
+                          <b>User Role :</b> Maintenance Manager
                         </span>
                       </div>
                     </MenuItem>
                     <hr />
-                    <MenuItem
-                      onClick={handleClose}
-                      // sx={{ borderBottom: "1px solid #000000" }}
-                    >
+                    <MenuItem onClick={handleClose}>
                       <SettingsOutlinedIcon sx={{ marginRight: "1rem" }} />
                       Settings
                     </MenuItem>
-                    <hr width="215rem" />
+                    <hr />
                     <MenuItem
                       onClick={handleClose}
                       sx={{
@@ -216,5 +120,3 @@ const TopBar = (props) => {
 };
 
 export default TopBar;
-
-
