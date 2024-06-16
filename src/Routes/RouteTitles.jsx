6fce6ecc6aa940ab967735e2d6f11829c1e98432
Praneeth Saranga <praneethskulathilaka@gmail.com>
2024-06-16 11:58:38 +0530

    User Authentication

---
+++ src/Routes/RouteTitles.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RouteTitles = {
+    '/dashboard' : 'Dashboard',
+    '/finance' : 'Finance',
+    '/finance/utilitycharges' : 'Utility Charges',
+    '/finance/expenses' : 'Expenses',
+    '/finance/revenue' : 'Revenues',
+    '/finance/warnings' : 'Warnings',
+    '/finance/editFunds' : 'Fund Types'
+
+}
+
+export default RouteTitles
